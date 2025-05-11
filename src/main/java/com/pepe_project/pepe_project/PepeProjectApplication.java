398ec2e4afd78ff5369e4ed8931814ebc18743f6
mariocofre <mari.cofrep@duocuc.cl>
2025-05-10 23:17:23 -0400

    Primeros pasos del pepe

---
+++ src/main/java/com/pepe_project/pepe_project/PepeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pepe_project.pepe_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pepe_project/pepe_project/PepeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pepe_project.pepe_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
